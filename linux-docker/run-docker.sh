bf36fad8c9ea29e5f70b6a8dc96154f9356860c3
jrrpanix <jreynoldsvc@gmail.com>
2025-05-18 18:51:45 +0400

    update

--- linux-docker/run-docker.sh
+++ linux-docker/run-docker.sh
@@ -5,7 +5,7 @@ WORKDIR=${WORKDIR:-$1}
 # Require WORKDIR to be passed as an environment variable
 if [ -z "$WORKDIR" ]; then
     echo "❌ Error: WORKDIR must be set before running this script."
-    echo "✅ Example: WORKDIR=/home/ubuntu/dev $0 or $0 /home/ubuntu/dev"
+    echo "✅ Example: WORKDIR=$HOME/dev $0 or $0 $HOME/dev"
     exit 1
 fi
 
