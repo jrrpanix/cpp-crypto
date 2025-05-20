daab54be5d73f4acdd230c634f4ce59874dc9651
jrrpanix <jreynoldsvc@gmail.com>
2025-05-20 07:13:19 +0400

    test_clock

---
+++ book-ticker-simd/test_clock_time.cpp
@@ -0,0 +1,26 @@
+#include "time_utils.hpp"
+#include <iostream>
+
+void benchmark_clock_calls() {
+    constexpr int N = 1'000'000;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    volatile int32_t sink = 0;  // prevent optimization
+    for (int i = 0; i < N; ++i) {
+        sink = sink + get_time_since_midnight_us();
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+    std::cout << "Total time: " << duration_ns << " ns\n";
+    std::cout << "Average time per call: " << (duration_ns / N) << " ns\n";
+}
+
+int main() {
+  benchmark_clock_calls();
+  return 0;
+}
+
+
