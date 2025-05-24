cb3606af6ab20cbce9c3e7fd7f089f187222a972
jrrpanix <jreynoldsvc@gmail.com>
2025-05-24 11:42:33 +0400

    rm

--- book-ticker-simd/run-build.sh
+++
@@ -1,4 +0,0 @@
-mkdir build && cd build
-cmake .. -DLOCAL_INCLUDE_DIR=/workspace/install/include -DLOCAL_LIB_DIR=/workspace/install/lib -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/bin/g++
-make
-
