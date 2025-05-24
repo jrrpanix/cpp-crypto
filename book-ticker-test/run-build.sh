f2a84ab9049349cff734dd71c1dde808b644c341
jrrpanix <jreynoldsvc@gmail.com>
2025-05-24 11:18:04 +0400

    rm old files

--- book-ticker-test/run-build.sh
+++
@@ -1,4 +0,0 @@
-mkdir build && cd build
-cmake .. -DIXWEBSOCKET_INCLUDE_DIR=/workspace/install/include -DIXWEBSOCKET_LIB_DIR=/workspace/install/lib
-make
-
--- cpp-docker/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-
-
-int main(int argc, char **argv) {
-	std::string a1 = argv[0];
-	std::cout << "the name of the executable is " << a1 << std::endl;
-}
