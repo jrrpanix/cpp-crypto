0c2bdd4c73ba67e25ed66cfa1c00cc30a7203a2d
jrrpanix <jreynoldsvc@gmail.com>
2025-05-25 14:11:34 +0400

    add build

---
+++ build_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+if [ "$1" = "cpp" ]; then
+    echo "🔨 Building C++-only dev image..."
+    docker build -t cpp-dev -f docker/cpp-dev/Dockerfile .
+elif [ "$1" = "full" ]; then
+    echo "🔨 Building full Python+C++ dev image..."
+    docker build -t full-dev -f docker/full-dev/Dockerfile .
+else
+    echo "Usage: $0 [cpp|full]"
+    exit 1
+fi
+
