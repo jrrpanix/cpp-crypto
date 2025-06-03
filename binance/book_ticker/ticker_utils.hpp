bdc73ae6a945ffd37b1d04a1c020acefe644804f
jrrpanix <jreynoldsvc@gmail.com>
2025-06-03 06:55:46 +0400

    mod

--- binance/book_ticker/ticker_utils.hpp
+++ binance/book_ticker/ticker_utils.hpp
@@ -8,14 +8,14 @@ inline double compute_mid_price(const BookTicker &bt) {
 }
 
 inline double compute_micro_price(const BookTicker &bt) {
-  double denom = bt.bid_price + bt.ask_price;
+  double denom = bt.bid_qty + bt.ask_qty;
   return (denom > 0)
              ? (bt.bid_qty * bt.ask_price + bt.ask_qty * bt.bid_price) / denom
              : std::numeric_limits<double>::quiet_NaN();
 }
 
 inline double compute_weighted_price(const BookTicker &bt) {
-  double denom = bt.bid_price + bt.ask_price;
+  double denom = bt.bid_qty + bt.ask_qty;
   return (denom > 0)
              ? (bt.bid_qty * bt.bid_price + bt.ask_qty * bt.ask_price) / denom
              : std::numeric_limits<double>::quiet_NaN();
